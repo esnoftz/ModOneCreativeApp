d65bcf18306fc70a78fa9717f4030423ed35c317
Evangeline Noftz <esnoftz@gmail.com>
2024-08-30 08:08:49 -0500

    Added text fields.

--- ModOneCreativeApp/ViewController.swift
+++ ModOneCreativeApp/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var takenOutTextField: UITextField!
+    
+    @IBOutlet weak var enteredTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
