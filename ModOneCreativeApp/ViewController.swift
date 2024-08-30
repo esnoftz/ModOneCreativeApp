5c2f4e413d84f4a1b320d2fe7635060492a6f145
Evangeline Noftz <esnoftz@gmail.com>
2024-08-30 07:48:23 -0500

    Initial Commit

---
+++ ModOneCreativeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModOneCreativeApp
+//
+//  Created by EVANGELINE NOFTZ on 8/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
